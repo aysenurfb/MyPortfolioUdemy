fb557c098a0f4a802cb12bca2a0b0d64d61e9b14
aktepeayse96 <aktepeayse96@gmail.com>
2024-04-17 21:36:23 +0300

    completed showing datas from the database

--- MyPortfolioUdemy/ViewComponents/_ExperienceComponentPartial.cs
+++ MyPortfolioUdemy/ViewComponents/_ExperienceComponentPartial.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
 
 namespace MyPortfolioUdemy.ViewComponents
 {
     public class _ExperienceComponentPartial:ViewComponent
     {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
         public IViewComponentResult Invoke()
         {
-            return View();
+            var myExperiences = portfolioContext.Experiences.ToList();
+            return View(myExperiences);
         }
     }
 }
