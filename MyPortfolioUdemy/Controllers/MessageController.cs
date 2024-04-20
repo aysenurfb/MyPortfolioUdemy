3348b3bb8f71ffc30dbbc09e6187505fec17a437
aktepeayse96 <aktepeayse96@gmail.com>
2024-04-20 22:18:10 +0300

    Finished coding to experience and messages components

---
+++ MyPortfolioUdemy/Controllers/MessageController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+
+namespace MyPortfolioUdemy.Controllers
+{
+	public class MessageController : Controller
+	{
+		MyPortfolioContext portfolioContext = new MyPortfolioContext();
+		public IActionResult Inbox()
+		{
+			var values = portfolioContext.Messages.ToList();
+			return View(values);
+		}
+
+		public IActionResult ChangeIsReadToTrue(int id)
+		{
+			var value = portfolioContext.Messages.Find(id);
+			value.IsRead = true;
+			portfolioContext.SaveChanges();
+			return RedirectToAction("Inbox");
+		}
+
+        public IActionResult ChangeIsReadToFalse(int id)
+        {
+            var value = portfolioContext.Messages.Find(id);
+            value.IsRead = false;
+            portfolioContext.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+		public IActionResult DeleteMessage(int id)
+		{
+			var value = portfolioContext.Messages.Find(id);
+			portfolioContext.Messages.Remove(value);
+			portfolioContext.SaveChanges();
+			return RedirectToAction("Inbox");
+		}
+
+		public IActionResult MessageDetail(int id)
+		{
+			var value = portfolioContext.Messages.Find(id);
+			return View(value);
+		}
+    }
+}
