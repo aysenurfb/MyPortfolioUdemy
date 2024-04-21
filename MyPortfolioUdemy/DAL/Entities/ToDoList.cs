867756f6cd70abea5aa0ad40b6a6d8ca68e74822
aktepeayse96 <aktepeayse96@gmail.com>
2024-04-21 09:06:05 +0300

    finishing the udemy course

---
+++ MyPortfolioUdemy/DAL/Entities/ToDoList.cs
@@ -0,0 +1,11 @@
+﻿namespace MyPortfolioUdemy.DAL.Entities
+{
+    public class ToDoList
+    {
+        public int ToDoListId { set; get; }
+        public string Tittle { set; get; }
+        public string ImageUrl { set; get; }
+        public DateTime Date { set; get; }
+        public bool Status { set; get; }
+    }
+}
